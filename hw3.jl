d00f1c29f911c8bc3f01adec66017ce113538b79
Caio Figueiredo <apoema.asa@gmail.com>
2019-10-15 19:24:57 -0400

    Q2

--- hw3.jl
+++ hw3.jl
@@ -10,6 +10,7 @@ y = CSV.read("y.csv", header=false)
 
 X = convert(Matrix, X)
 y = convert(Matrix, y)
+plot(y = y, x = X[:, 4], Geom.point)
 
 function logLik(β, X, y)
   # Sanity check
@@ -24,6 +25,38 @@ function logLik(β, X, y)
   return -logLikvalue
 end
 
-# Question 1
-optimize(β -> logLik(β, X, y), fill(0.4, 6), NelderMead())
+function ∇(β, X, y)
+  # Sanity check
+  if(size(X)[1] != size(y)[1])
+    error("X and y must have compatible dimensions")
+  end
 
+  G = zeros(6)
+  for i in 1:size(y)[1]
+    G += exp(X[i, :]' * β)*X[i,:] - y[i]*X[i,:]
+  end
+  return G
+end
+
+# Question 1
+res_1 = optimize(β -> logLik(β, X, y), fill(0.4, 6), NelderMead())
+min_1 = Optim.minimizer(res_1)
+
+# Question 2
+res_2 = optimize(β -> logLik(β, X, y), β -> ∇(β, X, y), [1.0;0;0;0;0;0], LBFGS(), Optim.Options(show_trace = true); inplace = false)
+min_2 = Optim.minimizer(res_2)
+
+# Test fully Newton
+
+function h(β, X)
+  H = zeros(6, 6)
+  for i in 1:size(X)[1]
+    H += exp(X[i, :]' * β)*X[i,:]*X[i,:]'
+  end
+  return H
+end
+
+res_new = optimize(β -> logLik(β, X, y), β -> ∇(β, X, y), β -> h(β, X), [1.0;0;0;0;0;0], Newton(), Optim.Options(show_trace = true); inplace = false)
+min_new = Optim.minimizer(res_new)
+
+#same as BFGS
