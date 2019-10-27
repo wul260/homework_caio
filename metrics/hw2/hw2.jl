a0ae797ea4cbd551f053aeb74037ba190480873a
Caio Figueiredo <apoema.asa@gmail.com>
2019-10-27 02:46:11 -0400

    metrics updates

--- metrics/hw2/hw2.jl
+++ metrics/hw2/hw2.jl
@@ -1,22 +1,29 @@
 using Distributions
 using DataFrames
 using Optim
-using Weave
 using JLD
 
 include("aux.jl")
 
+### Q1 {{{
 
-### Q2
+x = rand(Γ, 1000)
+y = exp.(x)
+p = plot(h -> (kernel(1, x, h, φ) - pdf(Γ, 1))^2, 0.1, 1)
+p = plot(h -> (kreg(1, x, y, h, φ) - exp(1))^2, 0.1, 1)
 
-# Defining Kernels
+
+### }}}
+
+### Q2 {{{
+# Defining Kernels {{{2
 k1(x) = φ(x)
 k2(x) = φ(x)*(3 - x^2)/2
 k3(x) = 2*φ(x) - φ(x/sqrt(2))/sqrt(2)
 k4(x) = sin(π*x)/π*x
 k = Dict(:1 => k1, :2 => k2, :3 => k3, :4 => k4)
-
-
+# }}}
+## Cross validation {{{2
 if isfile("Q2-cvs.jld")
   cv_value = load("Q2-cvs.jld")["cv_value"]
 else
@@ -32,7 +39,8 @@ else
   save("Q2-cvs.jld", "cv_value", cv_value)
 end
 
-## Q2 - a Bootstrap Simulation and Estimation
+## 2}}}}
+## Q2 - a {{{2
 res = DataFrame(n = Int16[], i = Int16[], h = Float64[], method=Symbol[], est = Float64[])
 for n in [10 100 1000], i in 1:1000 
   x = rand(Γ, n)
@@ -43,9 +51,10 @@ end
 
 res2 = by(res, [:n, :h, :method], :est => β -> MSE(β, pdf(Γ, 1)))
 
-draw(PNG("Q2-a.png", 1200px, 600px), q2_plot(res2, cv_value))
+draw(PNG("Q2-a.png", 16cm, 16cm), q2_plot(res2, cv_value))
 
-# Q2-b
+# 2}}}
+## Q2 - b {{{2
 if isfile("Q2-b.jld")
   q2b = load("Q2-b.jld")["q2b"]
 else
@@ -76,7 +85,7 @@ else
   q2b     = vcat(res2_2b, res3_2b)
   save("Q2-b.jld", "q2b", res4_2b)
 end
-draw(PNG("Q2-b.png", 1200px, 600px), q2_plot(q2b, cv_value))
+draw(PNG("Q2-b.png", 16cm, 16cm), q2_plot(q2b, cv_value))
 
-### EXPORT
-# weave("hw2.jmd", out_path=:pwd)
+### }}}
+### }}}
