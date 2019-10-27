69bd6a330b3f9865a374be5a1c8cc43bf0555d5f
Caio Figueiredo <apoema.asa@gmail.com>
2019-10-27 02:47:02 -0400

    readability improvements

--- metrics/hw2/hw2.jl
+++ metrics/hw2/hw2.jl
@@ -6,7 +6,6 @@ using JLD
 
 include("aux.jl")
 
-
 ### Q2
 
 # Defining Kernels
@@ -15,7 +14,6 @@ k2(x) = φ(x)*(3 - x^2)/2
 k3(x) = 2*φ(x) - φ(x/sqrt(2))/sqrt(2)
 k4(x) = sin(π*x)/π*x
 k = Dict(:1 => k1, :2 => k2, :3 => k3, :4 => k4)
-
 
 if isfile("Q2-cvs.jld")
   cv_value = load("Q2-cvs.jld")["cv_value"]
@@ -33,50 +31,47 @@ else
 end
 
 ## Q2 - a Bootstrap Simulation and Estimation
-res = DataFrame(n = Int16[], i = Int16[], h = Float64[], method=Symbol[], est = Float64[])
+k_est= DataFrame(n = Int16[], i = Int16[], h = Float64[], method=Symbol[], est = Float64[])
 for n in [10 100 1000], i in 1:1000 
   x = rand(Γ, n)
   for j in 1:4, h in 0.1:0.05:1.5
-    push!(res, (n, i, h, Symbol(j), kernel(1, x, h, k[j])))
+    push!(k_est, (n, i, h, Symbol(j), kernel(1, x, h, k[j])))
   end
 end
 
-res2 = by(res, [:n, :h, :method], :est => β -> MSE(β, pdf(Γ, 1)))
+k_mse = by(k_est, [:n, :h, :method], :est => β -> MSE(β, pdf(Γ, 1)))
 
-draw(PNG("Q2-a.png", 1200px, 600px), q2_plot(res2, cv_value))
+draw(PNG("Q2-a.png", 1200px, 600px), q2_plot(k_mse, cv_value))
 
 # Q2-b
 if isfile("Q2-b.jld")
   q2b = load("Q2-b.jld")["q2b"]
 else
-  res2b = DataFrame(n = Int16[], i = Int16[], h = Float64[], method=Symbol[], kest = Float64[])
+  r_est = DataFrame(n = Int16[], i = Int16[], h = Float64[], method=Symbol[], kest = Float64[])
   for n in [10 100 1000], i in 1:1000 
     x = rand(Γ, n)
     y = log.(x.^2 .+ 1)
     for j in 1:4, h in 0.1:0.05:1.5
-      push!(res2b, (n, i, h, Symbol(j), kreg(1, x, y, h, k[j])))
+      push!(r_est, (n, i, h, Symbol(j), kreg(1, x, y, h, k[j])))
     end
   end
 
-  res2_2b = by(res2b, [:n, :h, :method], :kest => β -> MSE(β, log(2)))
+  r_mse = by(r_est, [:n, :h, :method], :kest => β -> MSE(β, log(2)))
 
-  res2b_loclin = DataFrame(n = Int16[], i = Int16[], h = Float64[], loclin_0 = Float64[], loclin_1 = Float64[])
+  ll_est = DataFrame(n = Int16[], i = Int16[], h = Float64[], loclin_0 = Float64[], loclin_1 = Float64[])
   for n in [10 100 1000], i in 1:200
     x = rand(Γ, n)
     y = log.(x.^2 .+ 1)
     for h in 0.1:0.05:1.5
       row =  (n, i, h)
       row = tuple(row..., loclin(1, x, y, h, k[1])...)
-      push!(res2b_loclin, row)
+      push!(ll_est, row)
     end
   end
 
-  res3_2b = by(res2b_loclin, [:n, :h], :loclin_1 => β -> MSE(β, 1.0))
-  res3_2b.method = 5
-  q2b     = vcat(res2_2b, res3_2b)
+  ll_mse = by(ll_est, [:n, :h], :loclin_1 => β -> MSE(β, 1.0))
+  ll_mse.method = 5
+  q2b    = vcat(r_mse, ll_mse)
   save("Q2-b.jld", "q2b", res4_2b)
 end
 draw(PNG("Q2-b.png", 1200px, 600px), q2_plot(q2b, cv_value))
-
-### EXPORT
-# weave("hw2.jmd", out_path=:pwd)
